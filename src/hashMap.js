38c234a4ad3c04cf341e8f0f04b8f7dbb5c9abe3
Ross Clay <clay.rossclay@gmail.com>
2024-09-05 16:07:28 -0400

    initial commit

--- src/hashMap.js
+++ src/hashMap.js
@@ -1,10 +1,21 @@
 import { LinkedList } from "./linkedList";
 
 class HashMap {
-  constructor(name) {
-    this.id = Date.now().toString();
-    this.name = name;
-    this.buckets = [];
+  constructor() {
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+    this.bs = [];
+    for (let i = 0; i < this.capacity; i++) {
+      this.bs[i] = new LinkedList();
+    }
+  }
+
+  createBuckets(num) {
+    let b = [];
+    for (let i = 0; i < num; i++) {
+      b.push([]);
+    }
+    return b;
   }
 
   hash(key) {
@@ -18,30 +29,107 @@ class HashMap {
     return hashCode;
   }
 
-  set(key, value) {
-    // how can we deal with collisions? linked lists
-    if (!this.buckets[value]) {
-      this.buckets[value] = this.hash(key);
-    } else if (this.buckets[value]) {
-      this.buckets[value];
-    }
+  bucket(key) {
+    let h = hash(key);
+    return this.bs[h % this.bs.length];
   }
 
-  get(key) {}
+  entry(bucket, key) {
+    for (let e of bucket) {
+      if (e.key === key) {
+        return e;
+      }
+    }
+    return null;
+  }
 
-  has(key) {}
+  set(key, value) {
+    let index = this.hash(key) % 16;
+    if (index < 0 || index >= this.capacity) {
+      throw new Error("Trying to access index out of bound");
+    }
+    let current = this.bs[index].head;
+    if (this.bs[index].contains(key)) {
+      for (let i = 0; i < this.bs[index].find(key); i++) {
+        current = current.next;
+      }
+      current.value = value;
+    }
+    this.bs[index].append(key, value);
+  }
 
-  remove(key) {}
+  get(key) {
+    let index = this.hash(key) % 16;
+    if (!this.bs[index].contains(key)) return null;
+    let current = this.bs[index].head;
+    for (let i = 0; i < this.bs[index].find(key); i++) {
+      current = current.next;
+    }
+    return current.value;
+  }
 
-  length() {}
+  has(key) {
+    let index = this.hash(key) % this.capacity;
+    return this.bs[index].contains(key);
+  }
+
+  remove(key) {
+    let index = this.hash(key) % this.capacity;
+    if (!this.bs[index].contains(key)) return false;
+    this.bs[index].removeAt(this.bs[index].find(key));
+    return true;
+  }
+
+  length() {
+    const size = this.bs.reduce((sum, current) => {
+      return (sum += current.size());
+    }, 0);
+    return size;
+  }
 
   clear() {
-    this.buckets = [];
+    this.bs.forEach((item) => {
+      if (item.size()) {
+        for (let i = 0; i < item.size(); i++) {
+          item.head = null;
+        }
+      }
+    });
   }
 
-  keys() {}
+  keys() {
+    const keysArray = this.bs.reduce((res, current) => {
+      let currNode = current.head;
+      for (let i = 0; i < current.size(); i++) {
+        res.push(currNode.key);
+        currNode = currNode.next;
+      }
+      return res;
+    }, []);
+    return keysArray;
+  }
 
-  values() {}
+  values() {
+    const valuesArray = this.bs.reduce((res, current) => {
+      let currNode = current.head;
+      for (let i = 0; i < current.size(); i++) {
+        res.push(currNode.value);
+        currNode = currNode.next;
+      }
+      return res;
+    }, []);
+    return valuesArray;
+  }
+
+  entries() {
+    const keysArray = this.keys();
+    const valuesArray = this.values();
+    const entriesArray = [];
+    for (let i = 0; i < keysArray.length; i++) {
+      entriesArray[i] = { key: keysArray[i], value: valuesArray[i] };
+    }
+    return entriesArray;
+  }
 }
 
-export default HashMap;
+export { HashMap };
